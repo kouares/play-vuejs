6bf6c76dde630e564f0d561a0e4478926c73fb2b
kouares <ares.fanovaru@gmail.com>
2017-07-18 00:17:31 +0900

    change return value type Future[AnyContent]

--- play-vuejs/app/controllers/notebook/NotebookController.scala
+++ play-vuejs/app/controllers/notebook/NotebookController.scala
@@ -4,10 +4,13 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 import controllers.forms.NotebookForm.NotebookForm
+import modules.AppExecutionContext
 import play.api.Logger
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+
+import scala.concurrent.Future
 
 /**
   * Created by kouares on 2017/07/16.
@@ -25,22 +28,30 @@ object NotebookController {
 }
 
 @Singleton
-class NotebookController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
+class NotebookController @Inject()(cc: ControllerComponents)(implicit ec: AppExecutionContext) extends AbstractController(cc){
   private val logger = Logger(classOf[NotebookController])
 
-  def list() = Action { implicit request: Request[AnyContent] =>
-    Ok
+  def list() = Action.async { implicit request: Request[AnyContent] =>
+    Future {
+      Ok
+    }
   }
 
-  def create() = Action { implicit request: Request[AnyContent] =>
-    Ok
+  def create() = Action.async { implicit request: Request[AnyContent] =>
+    Future {
+      Ok
+    }
   }
 
-  def update() = Action { implicit request: Request[AnyContent] =>
-    Ok
+  def update() = Action.async { implicit request: Request[AnyContent] =>
+    Future {
+      Ok
+    }
   }
 
-  def remove() = Action { implicit request: Request[AnyContent] =>
-    Ok
+  def remove() = Action.async { implicit request: Request[AnyContent] =>
+    Future {
+      Ok
+    }
   }
 }
