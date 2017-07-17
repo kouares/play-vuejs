9e9b3399842f79b147453b57fa27dcc3579ea708
kouares <ares.fanovaru@gmail.com>
2017-07-18 00:18:22 +0900

    edit NotebookService

--- play-vuejs/app/services/NotebookService.scala
+++ play-vuejs/app/services/NotebookService.scala
@@ -1,8 +1,23 @@
 package services
 
+import controllers.forms.NotebookForm.NotebookForm
+import models.Notebook
+
+import scala.concurrent.Future
+
 /**
   * Created by koichi on 2017/07/17.
   */
 trait NotebookService {
+  def findByTitle(title: String): Future[Option[Notebook]]
 
+  def findAll(): Future[Seq[Notebook]]
+
+  def findAllBy(notebookForm: NotebookForm): Future[Seq[Notebook]]
+
+  def create(notebookForm: NotebookForm): Future[Notebook]
+
+  def save(notebookForm: NotebookForm): Future[Notebook]
+
+  def destroy(notebookForm: NotebookForm): Future[Int]
 }
--- play-vuejs/app/services/impl/NotebookServiceImpl.scala
+++ play-vuejs/app/services/impl/NotebookServiceImpl.scala
@@ -1,8 +1,63 @@
 package services.impl
 
+import java.sql.Timestamp
+import javax.inject.Inject
+
+import controllers.forms.NotebookForm.NotebookForm
+import models.Notebook
+import modules.AppExecutionContext
+import services.NotebookService
+
+import scala.concurrent.Future
+import scalikejdbc._
+import org.joda.time.DateTime
+
 /**
   * Created by koichi on 2017/07/17.
   */
-class NotebookServiceImpl {
+class NotebookServiceImpl @Inject()(implicit ec: AppExecutionContext) extends NotebookService with SQLSyntaxSupport[Notebook] {
 
+  def findByTitle(title: String): Future[Option[Notebook]] = {
+    Future {
+      Notebook.find(title)
+    }
+  }
+
+  def findAll(): Future[Seq[Notebook]] = {
+    Future {
+      Notebook.findAll()
+    }
+  }
+
+  def findAllBy(notebookForm: NotebookForm): Future[Seq[Notebook]] = {
+    Future {
+      Notebook.findAllBy(
+        sqls"""where
+               title like '%${notebookForm.title}%'
+               and mainText like '%${notebookForm.mainText}%'""")
+    }
+  }
+
+  def create(notebookForm: NotebookForm): Future[Notebook] = {
+    Future {
+      val current = new DateTime
+      Notebook.create(notebookForm.title, Some(notebookForm.mainText), Some(current), current)
+    }
+  }
+
+  def save(notebookForm: NotebookForm): Future[Notebook] = {
+    Future {
+      Notebook.find(notebookForm.title).map { notebook =>
+        notebook.copy(mainText = Some(notebookForm.mainText), upadtedAt = Some(new DateTime)).save()
+      }.getOrElse(throw new RuntimeException(s"更新対象[${notebookForm.title}]の記事が存在しません"))
+    }
+  }
+
+  def destroy(notebookForm: NotebookForm): Future[Int] = {
+    Future {
+      Notebook.find(notebookForm.title).map { notebook =>
+        notebook.destroy()
+      }.getOrElse(throw new RuntimeException(s"削除対象[${notebookForm.title}]の記事が存在しません"))
+    }
+  }
 }
