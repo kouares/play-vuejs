e942eaffee99dc0bd4a24d8c60f9c86e69e21593
kouares <ares.fanovaru@gmail.com>
2017-07-19 00:59:14 +0900

    edit TagMappingService bind

--- play-vuejs/app/modules/GlobalModule.scala
+++ play-vuejs/app/modules/GlobalModule.scala
@@ -1,15 +1,16 @@
 package modules
 
 import com.google.inject.AbstractModule
-import services.{NotebookService, TagMstService}
-import services.impl.{NotebookServiceImpl, TagMstServiceImpl}
+import services.{NotebookService, TagMappingService, TagMstService}
+import services.impl.{NotebookServiceImpl, TagMappingServiceImpl, TagMstServiceImpl}
 
 /**
   * Created by koichi on 2017/07/17.
   */
-class GrobalModule extends AbstractModule {
+class GlobalModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[NotebookService]).to(classOf[NotebookServiceImpl]).asEagerSingleton()
     bind(classOf[TagMstService]).to(classOf[TagMstServiceImpl]).asEagerSingleton()
+    bind(classOf[TagMappingService]).to(classOf[TagMappingServiceImpl]).asEagerSingleton()
   }
 }
