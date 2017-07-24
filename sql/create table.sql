e8f3ffff84eae719a1db5b80c881825bd93d515b
kouares <ares.fanovaru@gmail.com>
2017-07-25 00:08:40 +0900

    Fix create table.sql

--- sql/create table.sql
+++ sql/create table.sql
@@ -1,6 +1,6 @@
 create table play_catchup.notebook (
     id    int auto_increment primary key,
-    title varchar(190),
+    title varchar(190) not null,
     main_text varchar(10000),
     updated_at datetime,
     created_at datetime not null
@@ -17,5 +17,5 @@ create table play_catchup.tag_mapping (
     notebook_id int not null,
     tag_id int not null,
     foreign key (notebook_id) references notebook(id),
-    foreign key (tag_id) references tag_mst(id)
+    foreign key (tag_id) references tag(id)
 );
